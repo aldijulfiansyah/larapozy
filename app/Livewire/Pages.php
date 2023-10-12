a7029ab8662e3cf6d8e680a40deef1d71fdfc397
aldijulfiansyah <aldiksk@gmail.com>
2023-10-12 19:05:48 +0700

    create with validation done

--- app/Livewire/Pages.php
+++ app/Livewire/Pages.php
@@ -2,16 +2,92 @@
 
 namespace App\Livewire;
 use App\Models\Page;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Pages extends Component
 {
+
     public $slug;
     public $title;
     public $content;
+    public $modalVisible = false;
+    
+ 
+
+    
+    /**
+     * function for validation
+     *
+     * @return void
+     */
+    public function rules(){
+        return [
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('pages', 'slug')],
+            'content' => 'required',
+        ];
+    }
+
+
+    
+    /**
+     * melakukan input secara otomatis terhadap slug sesuai dengan title
+     * lalu regex menjadi str tanpa spasi dan kapital
+     * @return void
+     */
+    public function updateSlug(){
+        $this->slug = Str::slug($this->title);
+    }
+    
+
+
+   /**
+     * function create to DB
+     *
+     * @return void
+     */
+    
+    public function create(){
+        $this->validate();
+        Page::create($this->dataModel());
+        $this->modalVisible = false;
+        $this->clearValue();
+    }
+
+    /**
+     * this is data from Page Model
+     * used for mapping
+     *
+     * @return void
+     */
+    public function dataModel(){
+        return [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+        ];
+    }
+    
+    /**
+     * this will reset input of the livewire 
+     *
+     * @return void
+     */
+    public function clearValue(){
+        $this->title = null;
+        $this->slug = null;
+        $this->content = null;
+    }
+
 
     // modal set up
-    public $modalVisible = false;
+    public function closeCreateModal(){
+        
+         $this->modalVisible = false;
+    }
+
     
     /**
      * show the form modal
@@ -20,7 +96,7 @@ class Pages extends Component
      * @return void
      */
     public function showCreateModal(){
-        $this-> modalVisible = true;
+        $this->modalVisible = true;
     }
 
 
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -11,29 +11,32 @@
         <x-slot name="content">
             <div class="mt-4">
                 <x-label for="title" value="{{ __('Title') }}" />
-                <x-input id="title" class="block mt-1 w-full" type="text" wire:model.debounce.800ms="title" />
+                <x-input id="title" class="block mt-1 w-full" type="text" wire:model="title" wire:keyup="updateSlug" />
+                @error('title') <span class="text-red-600">{{$message}}</span> @enderror
             </div>
             <div class="mt-4">
                 <x-label for="slug" value="{{ __('Slug') }}" />
                 <div class="flex flex-row">
                     <span class="p-2 bg-gray-400 rounded-l-md shadow-sm flex text-black items-center">localhost:8000/</span>
-                    <input type="text" id="slug" class="rounded-r-md border-gray-300 shadow-sm w-full" wire:model.debounce.800ms="slug">
+                    <input type="text" id="slug" placeholder="url-slug" class="rounded-r-md border-gray-300 shadow-sm w-full" wire:model="slug" readonly>
                 </div>
+                @error('slug') <span class="text-red-600">{{$message}}</span> @enderror
             </div>
             <div class="mt-4">
                 <x-label for="content" value="{{ __('Content') }}" />
                 <div>
-                    <div class="mt-1 bg-white">
+                    <div class="mt-1 bg-white text-black">
                         <div class="body-content" wire:ignore>
                             <trix-editor 
                             class="trix-content"
                             x-ref="trix"
-                            wire:model.debouce.100000ms="content"
+                            wire:model.debounce.800ms="content"
                             wire:key="trix-content-unique-key"
                             ></trix-editor>
-                        </div>
+                        </div> 
                     </div>
                 </div>
+                @error('content') <span class="text-red-600">{{$message}}</span> @enderror
             </div>
 
             
